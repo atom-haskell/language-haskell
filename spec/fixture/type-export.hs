62b23ec4e1a285f2ba99a924b2df99bb2a5c4814
Nikolay Yakimov <root@livid.pp.ru>
2019-11-17 05:22:06 +0300

    Add spec for #125

---
+++ spec/fixture/type-export.hs
@@ -0,0 +1,8 @@
+module Test
+  ( (:-)(), apply,
+--          ^^^^^ meta.declaration.module meta.declaration.exports entity.name.function
+--      ^^ meta.declaration.module meta.declaration.exports meta.other.constructor-list
+--  ^^^^ meta.declaration.module meta.declaration.exports entity.name.function.operator
+  Type (..)
+--      ^^ meta.declaration.module meta.declaration.exports meta.other.constructor-list keyword.operator.wildcard
+  ) where
